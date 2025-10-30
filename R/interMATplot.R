af8b679bb94f169fc84599c971d7b0394b7eba81
nmary <n.mary@envt.fr>
2025-10-30 15:25:10 +0100

    interMATplot: first commit for a working function

--- R/interMATplot.R
+++ R/interMATplot.R
@@ -1,16 +1,16 @@
 #' @title Plot intra-chromosomal interaction matrix
 #'
-#' @description `interMATplot` allow to plot matrix with 2 types of annotations:
-#' * domains (e.g. TADs or compartments): plot as triangles and/or lines on the upper or/and lower part of the matrix.
-#' * interactions between domains/bins (e.g. loops): plot as squares on the upper and lower part of the matrix.
+#' @description `interMATplot` allow to plot non-symmetrical matrix with 2 types of annotations:
+#' * domains (e.g. TADs or compartments): plot as triangles and/or lines on the x or/and y axis of the matrix.
+#' * interactions between domains/bins (e.g. loops): plot as squares or rectangles.
 #'
 #' @details The matrix input must be a `Matrix` or a `matrix` object with interactions from 2 chromosome (i.e. inter-chromosomal interaction).
 #' All domains (TADs or compartments) are bed files (3 columns: chr, start and end) and can be R object (`dataframe` or `GRanges`) or the path of the files.
-#' For `tad.lines`, another column can be used to specify different classes of domains (e.g compartment A or B). To use those domain classes, specify the column number (of the `tad.upper.line` and `tad.lower.line` inputs) with `tad.line.col` parameter and a custom set of colors with `line.colors` parameter.
-#' Loop are stored in bedpe files (6 columns: chr1, start1, end, chr2, start2 and end2) and can be a `dataframe` object or the path of the file.
-#' Chromosome domains and loops can be filter using `tad.chr` parameter.
+#' For `tad.lines`, another column can be used to specify different classes of domains (e.g compartment A or B). To use those domain classes, specify the column number (from `tad.x.line` and `tad.y.line` inputs) with `tad.line.col` parameter and a custom set of colors with `line.colors` parameter.
+#' Loop are stored in bedpe files (6 columns: chr_y, start_y, end_y, chr_x, start_x and end_x) and can be a `dataframe` object or the path of the file.
+#' Chromosome domains and loops can be filter using `tad.chr.y` and `tad.chr.y` parameter.
 #'
-#' @param matrix `dgCMatrix` or `matrix` object with inter-chromosomal interactions.
+#' @param matrix non-symmetrical `dgCMatrix` or `matrix` object with inter-chromosomal interactions.
 #' @param bin.width Bin width of the matrix in base pair.
 #' @param start_y,stop_y Region of interest in base pair on y-axis. Default is NULL to plot all the chromosome.
 #' @param start_x,stop_x Region of interest in base pair on x-axis. Default is NULL to plot all the chromosome.
@@ -27,14 +27,13 @@
 #' * `"turbo"` (or `"H"`),
 #' * `"ObsExp"` (or `"OE"`),
 #' * `"ObsExp2"` (or `"OE2"`).
-#' @param tad.upper.tri,tad.lower.tri `data.frame`, `GRanges` or the bed files path with the TAD to plot as triangle in the upper or lower part of the matrix. Default is `NULL`.
-#' @param tad.upper.line,tad.lower.line `data.frame`, `GRanges` or the bed files path with the TAD to plot as line in the upper or lower parts of the matrix. Default is `NULL`.
-#' @param tad.line.col Column number of `tad.upper.line` and `tad.lower.line` files that contain factors used to color upper and lower lines. Default is `NULL`.
-#' @param loop.bedpe `data.frame` or bedpe files path to plot on both parts of the matrix. Six columns table (chr1, start1, end1, chr2, start2, end2) that gives loops between 2 areas. Default is `NULL`
-#' @param tad.chr Chromosome name to filter annotations (domains and loop). Default is `NULL`
+#' @param tad.y.tri,tad.x.tri `data.frame`, `GRanges` or the bed files path with the TAD to plot as triangle in the lower (x) or upper (y) part of the diagonal. Default is `NULL`.
+#' @param tad.y.line,tad.x.line `data.frame`, `GRanges` or the bed files path with the TAD to plot as line in the x or y axis of the matrix. Default is `NULL`.
+#' @param tad.line.col Column number of `tad.y.line` and `tad.x.line` files that contain factors used to color lines. Default is `NULL`.
+#' @param loop.bedpe `data.frame` or bedpe files path to plot on the matrix. Six columns table (chr_y, start_y, end_y, chr_x, start_x, end_x) that gives areas between 2 chromosomes. Default is `NULL`
+#' @param tad.chr.y,tad.chr.x Chromosome name to filter annotations (domains and loop). Default is `NULL`
 #' @param annotations.color Color for loop and triangular annotations. Default is `"red"`.
-#' @param line.colors Colors for `tad.upper.line` and `tad.lower.line`. Default is `c("red", "blue")`.
-
+#' @param line.colors Colors for `tad.x.line` and `tad.y.line`. Default is `c("red", "blue")`.
 #'
 #' @return `ggplot`
 #'
@@ -48,59 +47,15 @@
 #'
 #' @examples
 #'
-#' # get domains from boundaries:
-#' boundaries.gr = dataframes2grange(tad_HCT116_5kb.bed, human_chromsize)
-#' domains.gr = boundary2domain(boundaries.gr)
+#' # create a matrix 500x1000 bins
+#' matrix <- matrix(rnorm(500000, mean = 50, sd = 10) + c(rep(0, 250), seq(0, 50, length.out = 500), rep(0, 250)), 500, 1000)
 #'
-#' # get compartments from PC1:
-#' comp.gr = PC1calling(PC1_250kb.gr)
+#' interMATplot(matrix,
+#'     bin.width = 100e3)
 #'
-#' MATplot(mat_HCT116_chr19_50kb,
-#'     start = 5e6, stop = 15e6,
-#'     bin.width = 50e3, log2 = TRUE,
-#'     scale.colors = "H", #color of matrix, try "D" or "H"
-#'     tad.upper.tri = domains.gr,
-#'     tad.chr = "chr19")
-#'
-#'  # add compartement A and B
-#'  comp.gr = PC1calling(PC1_250kb.gr)
-#'
-#' MATplot(mat_HCT116_chr19_50kb,
-#'     start = 5e6, stop = 15e6,
-#'     bin.width = 50e3, log2 = TRUE,
-#'     scale.colors = "H", #color of matrix, try "D" or "H"
-#'     tad.upper.tri = domains.gr,
-#'     tad.upper.line = comp.gr,
-#'     tad.line.col = 1, #used first metadata column with compartments A or B
-#'     tad.chr = "chr19")
-
 interMATplot <- function(matrix, bin.width, start_y = NULL, stop_y = NULL, start_x = NULL, stop_x = NULL, log2 = T, scale.colors = "H", scale.limits = NULL,
-                    tad.upper.tri = NULL, tad.lower.tri = NULL, loop.bedpe = NULL, tad.chr = NULL, annotations.color = "red",
-                    tad.upper.line = NULL, tad.lower.line = NULL, tad.line.col = NULL, line.colors = c("red", "blue")) {
-
-  ######################################
-  cool.path = "~/mnt/genome3D/Var_struc/Bovin/Contact_maps/Distiller_Arima/norm/mcool/Bovin-669.ARS-UCD1.2.mapq_10.4000.mcool"
-  chr = 26
-  chr2 = 28
-  bin.width = 128000
-  balance = F; balancing_name = "weight"; verbose = TRUE
-  matrix = TADkit::cool2matrix(cool.path = cool.path, chr = chr, bin.width = bin.width, balance = balance, verbose = verbose, balancing_name = "weight", chr2 = chr2)
-  start_y = 1; stop_y = 128e4
-  start_x = 1; stop_x = 128e4
-  log2 = F
-  scale.colors = "D"
-  scale.limits = NULL
-  #[1,]  2 5 1 2 1 2 2 . 1 1
-  #[2,] 11 2 3 5 2 1 1 . 1 .
-  #[3,]  5 7 4 3 4 2 2 . 2 .
-  #[4,]  5 2 2 4 5 2 1 . 2 .
-  #[5,]  1 1 4 2 3 3 . 2 1 .
-  #[6,]  1 . 2 3 3 2 3 2 2 2
-  #[7,]  . 1 3 2 2 1 2 2 2 2
-  #[8,]  . 1 . 1 2 1 2 1 . .
-  #[9,]  5 5 1 2 6 1 1 . . .
-  #[10,]  3 5 2 4 2 2 2 3 . .
-  #########################################
+                    tad.x.tri = NULL, tad.y.tri = NULL, loop.bedpe = NULL, tad.chr_x = NULL, tad.chr_y = NULL, annotations.color = "red",
+                    tad.x.line = NULL, tad.y.line = NULL, tad.line.col = NULL, line.colors = c("red", "blue")) {
 
   #local variables:
   i <- j <- x <- e <- s <- chr <- e2 <- s2 <-start1 <- end1 <- start2 <- end2 <- chr1 <- chr2 <- NULL
@@ -133,13 +88,8 @@ interMATplot <- function(matrix, bin.width, start_y = NULL, stop_y = NULL, start
   #melt matrix
   melted_mat = Matrix::summary(mat) %>%
     dplyr::rename(c("y" = "i", "x" = "j", "i" = "x")) %>% #rename to avoid confusion
-    dplyr::mutate(x = (x + from2 - 1.5) * bin.width + 1) %>%  # genomic coordinates = center of the bin (needed for geom_tile)
-    dplyr::mutate(y = -(y + from1 - 1.5) * bin.width - 1)
-
-  #add genomic coordinates (i & j = bin starts)
-  #melted_mat = melted_mat %>% dplyr::rename(c("y" = "i", "x" = "j", "i" = "x")) #rename to avoid confusion
-  #melted_mat$x = (melted_mat$x + from2 - 1.5) * bin.width # genomic coordinates = center of the bin (needed for geom_tile)
-  #melted_mat$y = -(melted_mat$y + from1 - 1.5) * bin.width
+    dplyr::mutate(x = (x + from2 - 1.5) * bin.width) %>%  # genomic coordinates = center of the bin (needed for geom_tile)
+    dplyr::mutate(y = -(y + from1 - 1.5) * bin.width)
 
   #geom_tile
   p = ggplot2::ggplot()+ggplot2::geom_tile(data = melted_mat, ggplot2::aes(y = y, x = x, fill = i))+
@@ -159,85 +109,85 @@ interMATplot <- function(matrix, bin.width, start_y = NULL, stop_y = NULL, start
     p <- p + viridis::scale_fill_viridis(na.value = "black", option = scale.colors)
     }
 
-  #upper tri
-  if (!is.null(tad.upper.tri)) {
+  #tad.x.tri (horizontal axes)
+  if (!is.null(tad.x.tri)) {
 
-    if (is.character(tad.upper.tri)) {
-      tad = read.table(tad.upper.tri, header = F, sep = "\t")[,1:3]
+    if (is.character(tad.x.tri)) {
+      tad = read.table(tad.x.tri, header = F, sep = "\t")[,1:3]
     }
 
-    if (inherits(tad.upper.tri, "GRanges")) {
-      tad = as.data.frame(tad.upper.tri, row.names = NULL)[,1:3]
+    if (inherits(tad.x.tri, "GRanges")) {
+      tad = as.data.frame(tad.x.tri, row.names = NULL)[,1:3]
     }
 
-    if (is.data.frame(tad.upper.tri)) {
-      tad = tad.upper.tri[,1:3]
+    if (is.data.frame(tad.x.tri)) {
+      tad = tad.x.tri[,1:3]
     }
 
     names(tad) = c("chr", "s", "e")
-    if (is.null(tad.chr)) {
+    if (is.null(tad.chr_x)) {
       tad <- dplyr::filter(tad, e > start_x, s < stop_x)} else {
-        tad <- dplyr::filter(tad, chr == tad.chr, e > start, s < stop)}
+        tad <- dplyr::filter(tad, chr == tad.chr_x, e > start_x, s < stop_x)}
 
-    tad$e2 <- ifelse(tad$e >= stop, stop, tad$e)
-    tad$s2 <- ifelse(tad$s <= start, start, tad$s)
+    tad$e2 <- ifelse(tad$e >= stop_x, stop_x, tad$e)
+    tad$s2 <- ifelse(tad$s <= start_x, start_x, tad$s)
 
     p = p + ggplot2::geom_segment(data = tad, ggplot2::aes(x = s, y = -s, xend = e2, yend = -s2), color = annotations.color, size = 0.3)+
       ggplot2::geom_segment(data = tad, ggplot2::aes(x = e2, y = -s2, xend = e, yend = -e), color = annotations.color, size = 0.3)
   }
 
-  #lower tri
-  if (!is.null(tad.lower.tri)) {
+  #tad.y.tri (vertical axes)
+  if (!is.null(tad.y.tri)) {
 
-    if (is.character(tad.lower.tri)) {
-      tad = read.table(tad.lower.tri, header = F, sep = "\t")[,1:3]
+    if (is.character(tad.y.tri)) {
+      tad = read.table(tad.y.tri, header = F, sep = "\t")[,1:3]
     }
 
-    if (inherits(tad.lower.tri, "GRanges")) {
-      tad = as.data.frame(tad.lower.tri, row.names = NULL)[,1:3]
+    if (inherits(tad.y.tri, "GRanges")) {
+      tad = as.data.frame(tad.y.tri, row.names = NULL)[,1:3]
     }
 
-    if (is.data.frame(tad.lower.tri)) {
-      tad = tad.lower.tri[,1:3]
+    if (is.data.frame(tad.y.tri)) {
+      tad = tad.y.tri[,1:3]
     }
 
     names(tad) = c("chr", "s", "e")
-    if (is.null(tad.chr)) {
+    if (is.null(tad.chr_y)) {
       tad <- dplyr::filter(tad, e > start_y, s < stop_y)} else {
-        tad <- dplyr::filter(tad, chr == tad.chr, e > start_y, s < stop_y)}
+        tad <- dplyr::filter(tad, chr == tad.chr_y, e > start_y, s < stop_y)}
 
-    tad$e2 <- ifelse(tad$e >= stop, stop, tad$e)
-    tad$s2 <- ifelse(tad$s <= start, start, tad$s)
+    tad$e2 <- ifelse(tad$e >= stop_y, stop_y, tad$e)
+    tad$s2 <- ifelse(tad$s <= start_y, start_y, tad$s)
 
     p = p + ggplot2::geom_segment(data = tad, ggplot2::aes(x = s2, y = -e2, xend = e, yend = -e), color = annotations.color, size = 0.3)+
       ggplot2::geom_segment(data = tad, ggplot2::aes(x = s, y = -s, xend = s2, yend = -e2), color = annotations.color, size = 0.3)
   }
 
 
-  #upper line
-  if (!is.null(tad.upper.line)) {
+  #tad.x.line (horizontal lines)
+  if (!is.null(tad.x.line)) {
 
-    if (is.character(tad.upper.line)) {
-      tad = read.table(tad.upper.line, header = F, sep = "\t")[, c(1:3, tad.line.col)]
+    if (is.character(tad.x.line)) {
+      tad = read.table(tad.x.line, header = F, sep = "\t")[, c(1:3, tad.line.col)]
     }
 
-    if (inherits(tad.upper.line, "GRanges")) {
+    if (inherits(tad.x.line, "GRanges")) {
 
       if (is.null(tad.line.col)) {
-        tad = as.data.frame(tad.upper.line, row.names = NULL)[, c(1:3)]
+        tad = as.data.frame(tad.x.line, row.names = NULL)[, c(1:3)]
       } else {
-        temp = as.data.frame(tad.upper.line, row.names = NULL)
+        temp = as.data.frame(tad.x.line, row.names = NULL)
 
         if (length(temp) < (tad.line.col + 5)) {
-          stop(paste0("There is only ", length(temp) - 5, " column(s) with metadata in tad.upper.line input but you select column ", tad.line.col))
+          stop(paste0("There is only ", length(temp) - 5, " column(s) with metadata in tad.x.line input but you select column ", tad.line.col))
         }
 
         tad = temp[, c(1:3, tad.line.col + 5)]
       }
     }
 
-    if (is.data.frame(tad.upper.line)) {
-      tad = tad.upper.line[, c(1:3, tad.line.col)]
+    if (is.data.frame(tad.x.line)) {
+      tad = tad.x.line[, c(1:3, tad.line.col)]
     }
 
     if (is.null(tad.line.col)) {
@@ -246,43 +196,42 @@ interMATplot <- function(matrix, bin.width, start_y = NULL, stop_y = NULL, start
 
     names(tad) = c("chr", "s", "e", "col")
     tad$col = as.factor(tad$col)
-    if (is.null(tad.chr)) {
-      tad <- dplyr::filter(tad, e > start, s < stop)} else {
-        tad <- dplyr::filter(tad, chr == tad.chr, e > start, s < stop)
+    if (is.null(tad.chr_x)) {
+      tad <- dplyr::filter(tad, e > start_x, s < stop_x)} else {
+        tad <- dplyr::filter(tad, chr == tad.chr_x, e > start_x, s < stop_x)
       }
 
-    tad$e2 <- ifelse(tad$e >= stop, stop, tad$e)
-    tad$s2 <- ifelse(tad$s <= start, start, tad$s)
+    tad$e2 <- ifelse(tad$e >= stop_x, stop_x, tad$e)
+    tad$s2 <- ifelse(tad$s <= start_x, start_x, tad$s)
 
-    p = p + ggplot2::geom_segment(data = tad, ggplot2::aes(x = s2, y = -start, xend = e2, yend = -start, col = col), size = 1.5)+
-      ggplot2::geom_segment(data = tad, ggplot2::aes(x = stop, y = -s2, xend = stop, yend = -e2, col = col), size = 1.5)+
-      ggplot2::scale_colour_manual(values = line.colors)
+    p = p + ggplot2::geom_segment(data = tad, ggplot2::aes(x = s2, y = -start_y, xend = e2, yend = -start_y, col = col), size = 1.5)+
+      ggplot2::geom_segment(data = tad, ggplot2::aes(x = s2, y = -stop_y, xend = e2, yend = -stop_y, col = col), size = 1.5)
   }
 
-  #lower line
-  if (!is.null(tad.lower.line)) {
+  #tad.y.line
+  if (!is.null(tad.y.line)) {
 
-    if (is.character(tad.lower.line)) {
-      tad = read.table(tad.lower.line, header = F, sep = "\t")[, c(1:3, tad.line.col)]
+    if (is.character(tad.y.line)) {
+      tad = read.table(tad.y.line, header = F, sep = "\t")[, c(1:3, tad.line.col)]
     }
 
-    if (inherits(tad.lower.line, "GRanges")) {
+    if (inherits(tad.y.line, "GRanges")) {
 
       if (is.null(tad.line.col)) {
-        tad = as.data.frame(tad.lower.line, row.names = NULL)[, c(1:3)]
+        tad = as.data.frame(tad.y.line, row.names = NULL)[, c(1:3)]
       } else {
-        temp = as.data.frame(tad.lower.line, row.names = NULL)
+        temp = as.data.frame(tad.y.line, row.names = NULL)
 
         if (length(temp) < (tad.line.col + 5)) {
-          stop(paste0("There is only ", length(temp) - 5, " column(s) with metadata in tad.lower.line input but you select column ", tad.line.col))
+          stop(paste0("There is only ", length(temp) - 5, " column(s) with metadata in tad.y.line input but you select column ", tad.line.col))
         }
 
         tad = temp[, c(1:3, tad.line.col + 5)]
       }
     }
 
-    if (is.data.frame(tad.lower.line)) {
-      tad = tad.lower.line[, c(1:3, tad.line.col)]
+    if (is.data.frame(tad.y.line)) {
+      tad = tad.y.line[, c(1:3, tad.line.col)]
     }
 
     if (is.null(tad.line.col)) {
@@ -291,21 +240,20 @@ interMATplot <- function(matrix, bin.width, start_y = NULL, stop_y = NULL, start
 
     names(tad) = c("chr", "s", "e", "col")
     tad$col = as.factor(tad$col)
-    if (is.null(tad.chr)) {
-      tad <- dplyr::filter(tad, e > start, s < stop)} else {
-        tad <- dplyr::filter(tad, chr == tad.chr, e > start, s < stop)
+    if (is.null(tad.chr_y)) {
+      tad <- dplyr::filter(tad, e > start_y, s < stop_y)} else {
+        tad <- dplyr::filter(tad, chr == tad.chr_y, e > start_y, s < stop_y)
       }
 
-    tad$e2 <- ifelse(tad$e >= stop, stop, tad$e)
-    tad$s2 <- ifelse(tad$s <= start, start, tad$s)
+    tad$e2 <- ifelse(tad$e >= stop_y, stop_y, tad$e)
+    tad$s2 <- ifelse(tad$s <= start_y, start_y, tad$s)
 
-    if (is.null(tad.chr)) {
-      tad <- dplyr::filter(tad, e > start, s < stop)} else {
-        tad <- dplyr::filter(tad, chr == tad.chr, e > start, s < stop)}
+    p = p + ggplot2::geom_segment(data = tad, ggplot2::aes(x = start_x, y = -s2, xend = start_x, yend = -e2, col = col), size = 1)+
+      ggplot2::geom_segment(data = tad, ggplot2::aes(x = stop_x, y = -s2, xend = stop_x, yend = -e2, col = col), size = 1)
+  }
 
-    p = p + ggplot2::geom_segment(data = tad, ggplot2::aes(x = start, y = -s2, xend = start, yend = -e2, col = col), size = 1)+
-      ggplot2::geom_segment(data = tad, ggplot2::aes(x = s2, y = -stop, xend = e2, yend = -stop, col = col), size = 1)+
-      ggplot2::scale_colour_manual(values = line.colors)
+  if (!is.null(tad.x.line) | !is.null(tad.y.line)) {
+    p = p + ggplot2::scale_color_manual(values = line.colors)
   }
 
   #loop
@@ -319,14 +267,15 @@ interMATplot <- function(matrix, bin.width, start_y = NULL, stop_y = NULL, start
       loop = loop.bedpe[,1:6]
     }
 
-    names(loop) = c("chr1", "start1", "end1", "chr2", "start2", "end2")
+    names(loop) = c("c_y", "s_y", "e_y", "c_x", "s_x", "e_x")
     if (is.null(tad.chr)) {
-      loop <- dplyr::filter(loop, start1 >= start, end1 < stop, start2 > start, end2 < stop)} else {
-        loop <- dplyr::filter(loop, chr1 == tad.chr, chr2 == tad.chr, start1 >= start, end1 < stop, start2 > start, end2 < stop)}
+      loop <- dplyr::filter(loop, s_y >= start_y, e_y <= stop_y, s_x >= start_x, e_x <= stop_x)} else {
+        loop <- dplyr::filter(loop, c_y == tad.chr_y, c_x == tad.chr_x, s_y >= start_y, e_y <= stop_y, s_x >= start_x, e_x <= stop_x)}
 
 
-    p = p + ggplot2::geom_rect(data = loop, ggplot2::aes(xmin = start2, xmax = end2, ymin = -start1, ymax = -end1), fill = NA, color = annotations.color, size = 0.3)+
-      ggplot2::geom_rect(data = loop, ggplot2::aes(xmin = start1, xmax = end1, ymin = -start2, ymax = -end2), fill = NA, color = annotations.color, size = 0.3)
+    p = p +
+      ggplot2::geom_rect(data = loop, ggplot2::aes(xmin = s_x, xmax = e_x, ymin = -s_y, ymax = -e_y),
+                         fill = NA, color = annotations.color, size = 0.3)
   }
 
   p
